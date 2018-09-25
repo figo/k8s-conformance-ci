ea0feae0a124b7950daeab302c51ba28f327b7a8
hui luo <luoh@vmware.com>
2018-09-25 10:21:46 -0700

    update the GCS bucket folder structure

--- result.sh
+++ result.sh
@@ -5,7 +5,7 @@
 cd "${TESTGRID_UPLOAD}" || exit 1
 python /cncf/upload_e2e.py --junit="${CONFORMANCE_LOG_DIR}"/artifacts/junit_*.xml \
 --log="${CONFORMANCE_LOG_DIR}"/e2e.log \
---bucket=gs://k8s-conformance-cloud-provider-vsphere \
+--bucket=gs://k8s-conformance-cloud-provider-vsphere/head/ci/latest \
 --key-file="${TESTGRID_UPLOAD}"/key
 
 echo "test upload finished"
