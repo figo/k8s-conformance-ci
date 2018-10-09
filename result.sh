8d887dc58f29464685a11441ca806732585209cb
hui luo <luoh@vmware.com>
2018-10-09 07:43:48 -0700

    uploading result to experimental folder

--- result.sh
+++ result.sh
@@ -14,7 +14,7 @@ fi
 cd "${TESTGRID_UPLOAD}" || exit 1
 python /cncf/upload_e2e.py --junit="${CONFORMANCE_LOG_DIR}"/artifacts/junit_*.xml \
 --log="${CONFORMANCE_LOG_DIR}"/e2e.log \
---bucket=gs://k8s-conformance-cloud-provider-vsphere/head/"${K8S_VERSION_BASE}"/"${K8S_VERSION}" \
+--bucket=gs://k8s-conformance-cloud-provider-vsphere/experimental/head/"${K8S_VERSION_BASE}"/"${K8S_VERSION}" \
 --key-file="${TESTGRID_UPLOAD}"/key
 
 echo "test upload finished"
